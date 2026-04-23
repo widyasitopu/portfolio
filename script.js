28cfaaf0e53c9bbed770bb9e7fdf8c884534c028
widyasitopu <widyasitopu@students.polmed.ac.id>
2026-04-23 17:01:27 +0700

    build home section

---
+++ script.js
@@ -0,0 +1,35 @@
+const roles = [
+  "occasionally a funny lecturer",
+  "mostly a serious researcher",
+  "a student who probably should study... again",
+  "an analyst who finds what's hidden",
+  "a midnight reader who overthinks",
+];
+
+let i = 0;
+let j = 0;
+let isDeleting = false;
+const speed = 60;
+
+const textElement = document.querySelector(".changing");
+
+function typeEffect() {
+  const current = roles[i];
+
+  if (isDeleting) {
+    textElement.textContent = current.substring(0, j--);
+  } else {
+    textElement.textContent = current.substring(0, j++);
+  }
+
+  if (!isDeleting && j === current.length) {
+    setTimeout(() => (isDeleting = true), 1200);
+  } else if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+  }
+
+  setTimeout(typeEffect, isDeleting ? speed / 2 : speed);
+}
+
+typeEffect();
